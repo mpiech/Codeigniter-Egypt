9ea0535d37470f55913b715d77b6fa193ef58378
Mike Piech <mpiech@alum.mit.edu>
2011-09-12 10:07:23 -0700

    Edited config.php via GitHub

--- config.php
+++ config.php
@@ -1,8 +1,8 @@
 <?php
 $config_site_name		= "Site Name";
 
-$db_hostname			= "localhost";
+$db_hostname			= "10.194.111.8";
 $db_database			= "egypt";
-$db_username			= "root";
-$db_password			= "1234";
+$db_username			= "user_c648a135";
+$db_password			= "u4iC^G;-LRCXCs";
 $db_dbdriver			= "mysql";
